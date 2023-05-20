46e1dd339166e7e13d9689887f955336541057c9
Trang Truong <trang@hei.io>
2023-05-20 13:41:43 +0700

    update logic push notification except current user

--- app/jobs/push_new_video_notification_job.rb
+++ app/jobs/push_new_video_notification_job.rb
@@ -4,7 +4,7 @@ class PushNewVideoNotificationJob < ApplicationJob
   def perform(video)
     return unless video
     client = Firebase::Client.new(ENV['FIREBASE_URL'], ENV['FIREBASE_SECRET_KEY'])
-    User.all.each do |user|
+    User.where.not(id: video.user_id).each do |user|
       client.set("notifications/#{user.id}", { id: video.id, user_email: video.user.email, title: video.title})
     end
   end
